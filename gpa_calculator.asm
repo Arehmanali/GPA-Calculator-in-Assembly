111b98fd1dfba58b6a54d9b32ad6f6c13185c267
Rehman Ali <55204040+Arehmanali@users.noreply.github.com>
2019-12-16 21:44:29 -0800

    Update gpa_calculator.asm

--- gpa_calculator.asm
+++ gpa_calculator.asm
@@ -1,123 +1,120 @@
-
 INCLUDE Irvine32.inc 
 
 .data
-inputGrades BYTE "Please enter the grade of subject:"
-creditHour DWORD 25 DUP(?)
-gpa DWORD ?
-creditH DWORD 0
-sum DWORD 0
+inputGrades BYTE "Please enter the grade of subject:",0
+creditHour dword 25 DUP(?)
+gpa REAL4 ?
+creditH byte 0
+sum REAL4 0
 grade BYTE ?
-inputCredit BYTE "Please enter the credit Hours of Previous entered subject:"
+inputCredit BYTE "Please enter the credit Hours of Previous entered subject:",0
 gradeList BYTE 20 DUP(?)
-numOfSubjects DWORD ?
+numOfSubjects dword ?
 
 
-inputSubjects BYTE "Please enter the number of Subjects: "
+inputSubjects BYTE "Please enter the number of Subjects: ",0
 .code 
-
-inputGrade proto, numOfSub:dword, crdHrList:ptr dword, crdtHr:dword, gradList:ptr dword, inputGrads:byte, inputCreditH:byte
-evaluate proto, numOfSubjects:dword, grade:byte,gradeList:ptr byte,sum:dword,creditHour:ptr dword
 main PROC
 
 mov edx,offset inputSubjects
 call writeString
 call readInt
 mov numOfSubjects,eax
-
-invoke inputGrade, numOfSubjects, addr creditHour, creditH, addr gradeList, inputGrades, inputCredit
-invoke evaluate, numOfSubjects, grade,addr gradeList,sum,addr creditHour
+call inputGrade
+call evaluate
+call writeInt
 call readInt
 exit 
 main ENDP
 
-inputGrade PROC,numOfSub:dword, crditHour:ptr dword, crdtHr:dword, gradList:ptr dword, inputGrads:byte, inputCreditH:byte
-mov ecx, numOfSubjects
+inputGrade PROC
+mov  ecx, numOfSubjects
 mov esi, 0
 mov edi, 0
 L1:
 mov edx,offset inputGrades
 call writeString
-call readChar
-mov gradList[si], al
+call readstring
+call crlf
+mov gradeList[si], al
 
 mov edx, offset inputCredit
 call writeString
 call readInt
-mov crditHour[edi], eax
-add creditH, eax
+mov creditHour[edi], eax
+add creditH, al
 inc esi
-add edi,4
+inc edi
 Loop L1
 ret
  inputGrade ENDP
 
-evaluate PROC, numOfSub:dword, grad:byte, gradList:ptr byte,sm:dword,crditHour:ptr dword
+evaluate PROC
 mov ecx,numOfSubjects
 mov esi,0
 mov edi,0
 LOOP1:
-mov eax,gradList[esi]
-mov grad,al
-.IF grad[edi]=='A' && grad[edi*1]=='0dh'
-mov eax,crditHour[esi*4]
+mov al,gradeList[esi]
+mov grade,al
+.IF grade[edi]=='A' && grade[edi+1]==' '
+mov eax,creditHour[esi+4]
 mov ebx,4
 mul ebx
-add sm, eax
+add sum, eax
 
-.ELSEIF grad[edi]=='A' && grad[edi*1]=='+'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='A' && grade[edi+1]=='+'
+mov eax,creditHour[esi+4]
 mov ebx,4
 mul ebx
-add sm, eax
+add sum, eax
 
-.ELSEIF grad[edi]=='A' && grad[edi*1]=='-'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='A' && grade[edi+1]=='-'
+mov eax,creditHour[esi+4]
 mov ebx,3.7
 mul ebx
 add sm, eax
 
-.ELSEIF grad[edi]=='B' && grad[edi*1]=='0dh'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='B' && grade[edi+1]=='0dh'
+mov eax,creditHour[esi+4]
 mul 3.0
 add sm, eax
 
-.ELSEIF grad[edi]=='B' && grad[edi*1]=='+'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='B' && grade[edi+1]=='+'
+mov eax,creditHour[esi+4]
 mul 3.3
 add sm, eax
 
-.ELSEIF grad[edi]=='B' && grad[edi*1]=='-'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='B' && grade[edi+1]=='-'
+mov eax,creditHour[esi+4]
 mul 2.7
 add sm, eax
 
-.ELSEIF grad[edi]=='C' && grad[edi*1]=='+'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='C' && grade[edi+1]=='+'
+mov eax,creditHour[esi+4]
 mul 2.3
 add sm, eax
 
-.ELSEIF grad[edi]=='C' && grad[edi*1]=='0dh'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='C' && grade[edi+1]==''
+mov eax,creditHour[esi+4]
 mul 2.0
 add sm, eax
 
-.ELSEIF grad[edi]=='C' && grad[edi*1]=='-'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='C' && grade[edi+1]=='-'
+mov eax,creditHour[esi+4]
 mul 1.7
 add sm, eax
 
-.ELSEIF grad[edi]=='D' && grad[edi*1]=='+'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='D' && grade[edi+1]=='+'
+mov eax,creditHour[esi+4]
 mul 1.3
 add sm, eax
 
-.ELSEIF grad[edi]=='D' && grad[edi*1]=='0dh'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='D' && grade[edi+1]==' '
+mov eax,creditHour[esi+4]
 add sm, eax
 
-.ELSEIF grad[edi]=='F' && grad[edi*1]=='0dh'
-mov eax,crditHour[esi*4]
+.ELSEIF grade[edi]=='F' && grade[edi+1]==' '
+mov eax,creditHour[esi+4]
 mov ebx,0
 mul ebx
 add sm, eax
